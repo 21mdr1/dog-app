52e18fa78b9d79584ed18755f6ac3ef487c47100
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-07 00:11:54 -0500

    Add top bar structure

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './TopNav.scss';
 
 function TopNav() {
+    let [ avatar, setAvatar ] = useState('');
+
+    useEffect(() => {
+        setAvatar('https://api.multiavatar.com/maria.svg');
+    }, []);
+
     return (
-        <p>Top Nav</p>
+        <div className="top-nav">
+            <Link to='/user'>
+                <img src={avatar} alt="user avatar" className="top-nav__avatar" />
+            </Link>
+        </div>
     );
 }
 
