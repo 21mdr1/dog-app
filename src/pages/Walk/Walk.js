631fda5618bf9e75d768db388d2171420d976b46
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-08 11:47:11 -0500

    Make form functional

--- src/pages/Walk/Walk.js
+++ src/pages/Walk/Walk.js
@@ -6,6 +6,27 @@ import './Walk.scss';
 
 function Walk() {
     let [ displayForm, setDisplayForm ] = useState(false);
+    let [ inputs, setInputs ] = useState({hours: '0', minutes: '2', seconds: '0'})
+    let { hours, minutes, seconds } = inputs;
+
+    function clickOut() {
+        setDisplayForm(false);
+    }
+
+    function dontClickOut(event) {
+        event.stopPropagation();
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        //other logic
+        setDisplayForm(false);
+    }
+
+    function handleInputChange(event) {
+        setInputs({...inputs, [event.target.name]: event.target.value})
+    }
+
 
     return (
         <div className='page page--walk'>
@@ -13,21 +34,25 @@ function Walk() {
             <WalkingDog />
             <BottomNav page='walk' clickHandler={() => {setDisplayForm(true)}} />
         {displayForm && (
-            <div className='form__background'>
-                <form className="form">
+            <div className='form__background' onClick={clickOut}>
+                <form className="form" onClick={dontClickOut} onSubmit={submitHandler}>
                     <div className="form__content">
-                        <select className="form__input" defaultValue='2'>
+                        <select className="form__input" name='hours' value={hours} onChange={handleInputChange}>
+                            <option value="0" className="form__option">0</option>
                             <option value="1" className="form__option">1</option>
                             <option value="2" className="form__option">2</option>
                             <option value="3" className="form__option">3</option>
+                            <option value="4" className="form__option">4</option>
                         </select>
                         <p className="form__text">:</p>
-                        <select className="form__input" defaultValue='2'>
+                        <select className="form__input" name='minutes' value={minutes} onChange={handleInputChange}>
+                            <option value="0" className="form__option">0</option>
                             <option value="1" className="form__option">1</option>
                             <option value="2" className="form__option">2</option>
                         </select>
                         <p className="form__text">:</p>
-                        <select className="form__input">
+                        <select className="form__input" name='seconds' value={seconds} onChange={handleInputChange}>
+                            <option value="0" className="form__option">0</option>
                             <option value="1" className="form__option">1</option>
                             <option value="2" className="form__option">2</option>
                         </select>
