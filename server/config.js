0ad7a2b5ce38b6084bb936d4687881b69967c9a5
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-14 21:18:07 -0500

    Create users, steps, and preferences tables

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 const config = {
     db: {
         host: DB_HOST,
+        port : DB_PORT,
         user: DB_USER,
         password: DB_PASS,
         database: DB_NAME,
---
+++ server/migrations/1705282600536-steps.js
@@ -0,0 +1,12 @@
+
+let up = `CREATE TABLE steps (
+    steps_id int unsigned NOT NULL AUTO_INCREMENT,
+    timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    steps int unsigned NOT NULL DEFAULT 0,
+    mins_walked int unsigned NOT NULL DEFAULT 0,
+    user_id int unsigned NOT NULL,
+    PRIMARY KEY (steps_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);`
+
+let down = `DROP TABLE steps;`
