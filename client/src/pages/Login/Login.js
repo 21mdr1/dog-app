39805adff5b36af844014e79f03534ff2bdb2951
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-16 00:04:34 -0500

    Made register and login forms into controlled elements

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,8 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import back from '../../assets/icons/left_line.svg';
 import './Login.scss';
 
 function Login() {
+    const defaults = {
+        email: '',
+        password: '',
+    }
+    let [ inputs, setInputs ] = useState(defaults);
+    let { email, password } = inputs;
+
+    function handleInputChange(event) {
+        let { name, value } = event.target;
+        setInputs({...inputs, [name]: value});
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return (
         <div className='page page--login'>
             <Link to='/' className="back-button back-button--login">
@@ -13,19 +31,23 @@ function Login() {
                 />
                 <span className="back-button__text back-button__text--login">Back</span>
             </Link>
-            <form className='login-form'>
+            <form className='login-form' onSubmit={handleSubmit}>
                 <h1 className='login-form__title'>Welcome Back!</h1>
                 <input 
                     type="text" 
                     name='email' 
                     className="login-form__input" 
                     placeholder='Email'
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input 
                     type="password" 
                     name='password' 
                     className="login-form__input"
-                    placeholder='Password' 
+                    placeholder='Password'
+                    value={password}
+                    onChange={handleInputChange} 
                 />
                 <button className="login-form__button">
                     Sign In
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,8 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import back from '../../assets/icons/left_line.svg';
 import './Register.scss';
 
 function Register() {
+    const defaults = {
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: ''
+    }
+    let [ inputs, setInputs ] = useState(defaults);
+    let { email, username, password, confirmPassword } = inputs;
+
+    function handleInputChange(event) {
+        let { name, value } = event.target;
+        setInputs({...inputs, [name]: value});
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return (
         <div className='page page--register'>
             <Link to='/' className="back-button back-button--register">
@@ -13,31 +33,39 @@ function Register() {
                 />
                 <span className="back-button__text back-button__text--register">Back</span>
             </Link>
-            <form className='register-form'>
+            <form className='register-form' onSubmit={handleSubmit}>
                 <h1 className='register-form__title'>Create an Account</h1>
                 <input 
                     type="text" 
                     name='email' 
                     className="register-form__input" 
                     placeholder='Email'
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input 
                     type="text" 
                     name='username' 
                     className="register-form__input" 
                     placeholder='Username'
+                    value={username}
+                    onChange={handleInputChange}
                 />
                 <input 
                     type="password" 
                     name='password' 
                     className="register-form__input"
-                    placeholder='Password' 
+                    placeholder='Password'
+                    value={password}
+                    onChange={handleInputChange} 
                 />
                 <input 
                     type="password" 
                     name='confirmPassword' 
                     className="register-form__input" 
                     placeholder='Confirm password'
+                    value={confirmPassword}
+                    onChange={handleInputChange}
                 />
                 <button className="register-form__button">
                     Create Account
