d68e3063f2a62a04ad21ac54743299bfaf4812a5
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-15 23:06:27 -0500

    Create login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,7 +1,41 @@
+import { Link } from 'react-router-dom';
+import back from '../../assets/icons/left_line.svg';
 import './Login.scss';
 
 function Login() {
-
+    return (
+        <div className='page page--login'>
+            <Link to='/' className="back-button back-button--login">
+                <img 
+                    src={back} 
+                    alt="back" 
+                    className="back-button__image back-button__image--login" 
+                />
+                <span className="back-button__text back-button__text--login">Back</span>
+            </Link>
+            <form className='login-form'>
+                <h1 className='login-form__title'>Welcome Back!</h1>
+                <input 
+                    type="text" 
+                    name='email' 
+                    className="login-form__input" 
+                    placeholder='Email'
+                />
+                <input 
+                    type="password" 
+                    name='password' 
+                    className="login-form__input"
+                    placeholder='Password' 
+                />
+                <button className="login-form__button">
+                    Sign In
+                </button>
+                <p className='login-form__text'>Don't have an account? 
+                    <Link to='/register' className='login-form__text login-form__text--highlight'> Sign up</Link>
+                </p>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
