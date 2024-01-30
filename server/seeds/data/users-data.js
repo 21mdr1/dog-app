95adfd17d1aed0e9c51af82cf7ba132ea51b0fda
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-30 12:25:42 -0500

    Create seed data

---
+++ server/seeds/data/users-data.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {
+        username: "testUser",
+        password: "test123",
+        email: "test1@example.com",
+    }
+];
---
+++ server/seeds/seeds.js
@@ -0,0 +1,14 @@
+const userSeed = require('./data/users-data');
+const stepsSeed = require('./data/steps-data');
+const preferencesSeed = require('./data/prefereces-data');
+
+// let {username, email, password} = request.body;
+
+// let sql = `INSERT INTO users(username, email, password)
+//     values (?, ?, ?)`;
+// let inserts = [username, email, password];
+
+// const connection = await mysql.createConnection(config.db);
+// let [result, ] = await connection.query(sql, inserts);
+
+// response.json(result);
