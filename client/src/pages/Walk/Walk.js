942231988decc7746f725862e5f5776cce7a9420
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-30 18:15:19 -0500

    Move sun to its own component

--- client/src/pages/Walk/Walk.js
+++ client/src/pages/Walk/Walk.js
@@ -1,15 +1,10 @@
 import { useState } from 'react';
 import BottomNav from '../../components/BottomNav/BottomNav';
+import { ReactComponent as Tree } from '../../assets/static-images/tree-4.svg';
+import Sun from '../../components/Sun/Sun';
 import AnimatedWalkingDog from '../../components/AnimatedWalkingDog/AnimatedWalkingDog';
 import TopNav from '../../components/TopNav/TopNav';
 import WalkForm from '../../components/WalkForm/WalkForm';
-import tree from '../../assets/static-images/tree-4.svg';
-import sun1 from '../../assets/animations/sun/sun1.svg';
-import sun2 from '../../assets/animations/sun/sun2.svg';
-import sun3 from '../../assets/animations/sun/sun3.svg';
-import sun4 from '../../assets/animations/sun/sun4.svg';
-import sun5 from '../../assets/animations/sun/sun5.svg';
-import sun6 from '../../assets/animations/sun/sun6.svg';
 import './Walk.scss';
 
 function Walk() {
@@ -17,15 +12,8 @@ function Walk() {
 
     return (
         <div className='page page--walk'>
-            <div className="sun">
-                <img src={sun1} alt="" className="sun__layer1" />
-                <img src={sun2} alt="" className="sun__layer2" />
-                <img src={sun3} alt="" className="sun__layer3" />
-                <img src={sun4} alt="" className="sun__layer4" />
-                <img src={sun5} alt="" className="sun__layer5" />
-                <img src={sun6} alt="" className="sun__layer6" />
-            </div>
-            <img src={tree} alt='tree' className="page__background-tree" />
+            <Sun />
+            <Tree className="page__background-tree" />
             <TopNav page='walk'/>
             <AnimatedWalkingDog />
             <BottomNav page='walk' clickHandler={() => {setDisplayForm(true)}} />
