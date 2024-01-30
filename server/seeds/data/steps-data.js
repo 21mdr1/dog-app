baa9ba10fffc19600eb2f8cf0529fc39fb3f7df2
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-30 13:09:09 -0500

    Fix dates in steps seed data

--- server/seeds/data/steps-data.js
+++ server/seeds/data/steps-data.js
@@ -21,67 +21,67 @@ module.exports = [
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 0) / 1000),
         steps: 3600,
         mins_walked: 60,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 2) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 300,
         mins_walked: 5,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 2) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 0) / 1000),
         steps: 5400,
         mins_walked: 90,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 3) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 1800,
         mins_walked: 30,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 3) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 0) / 1000),
         steps: 900,
         mins_walked: 15,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 4) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 7200,
         mins_walked: 120,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 4) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 0) / 1000),
         steps: 3600,
         mins_walked: 60,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 5) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 900,
         mins_walked: 15,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 5) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 0) / 1000),
         steps: 900,
         mins_walked: 15,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 6) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 1200,
         mins_walked: 20,
         user_id: 1
     },
     {
-        entry_logged: Math.floor(d.setDate(d.getDate() - 7) / 1000),
+        entry_logged: Math.floor(d.setDate(d.getDate() - 1) / 1000),
         steps: 300,
         mins_walked: 5,
         user_id: 1
