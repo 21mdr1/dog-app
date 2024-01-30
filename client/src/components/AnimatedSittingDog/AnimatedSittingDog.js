7455b02188750806b4ac094d854172f0f98e238a
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-30 17:59:38 -0500

    Add blinking background animation

--- client/src/components/AnimatedSittingDog/AnimatedSittingDog.js
+++ client/src/components/AnimatedSittingDog/AnimatedSittingDog.js
@@ -1,9 +1,26 @@
+import { moveNaturally, blink } from '../../utils/animationUtils';
 import { useState, useEffect } from 'react';
-import './AnimatedSittingDog.scss';
 import SittingDog from '../SittingDog/SittingDog';
+import './AnimatedSittingDog.scss';
+
 
 function AnimatedSittingDog() {
     let [ isBlinking, setIsBlinking ] = useState(false);
+    // need to straighten out some animations before I can use these
+    // let [ isBobbingHead, setIsBobbingHead ] = useState(false);
+    // let [ isWaggingTail, setIsWaggingTail ] = useState(false);
+
+    useEffect(() => {
+        let intervals = [];
+
+        intervals.concat(moveNaturally(blink, setIsBlinking));
+
+        return () => {
+            for (let interval of intervals) {
+                clearInterval(interval);
+            }
+        }
+    }, []);
 
     return (
         <SittingDog isBlinking={isBlinking} />
