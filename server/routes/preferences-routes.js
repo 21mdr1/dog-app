3bf5993394e96d4bc72820052ef5780303bf6a2c
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-30 21:39:35 -0500

    Delete commented out code

--- server/routes/preferences-routes.js
+++ server/routes/preferences-routes.js
@@ -6,22 +6,7 @@ router.route('/')
     .post(preferencesController.createPreferences)
     .patch(preferencesController.changePreferences);
 
-
 router.route('/:userId')
     .get(preferencesController.getPreferences);
-
-// router.patch('/', async (request, response) => {
-//     let {preference, value, userId} = request.body;
-
-//     let sql = `UPDATE preferences
-//         SET ${preference} = ?
-//         WHERE user_id = ?;`;
-//     let inserts = [value, userId];
-
-//     const connection = await mysql.createConnection(config.db);
-//     let [result, _fields] = await connection.query(sql, inserts);
-
-//     response.json(result);
-// });
 
 module.exports = router;
