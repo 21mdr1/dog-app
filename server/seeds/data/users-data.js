e8df5072cd2e1cd94f9ae65c4a3002af706ae2b8
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-31 21:36:14 -0500

    Hash password in user seed

--- server/seeds/data/users-data.js
+++ server/seeds/data/users-data.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = process.env;
+
 module.exports = [
     {
         user_id: 1,
         username: "testUser",
-        password: "test123",
+        password: bcrypt.hash("test123", Number(SALT_ROUNDS)),
         email: "test1@example.com",
     }
 ];
