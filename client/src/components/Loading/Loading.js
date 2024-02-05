57ba46aa5497d5a5f59651bdbaf1fc508c9362a8
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-04 21:23:40 -0500

    Style not found page

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -1,4 +1,4 @@
-import './Loading.js';
+import './Loading.scss';
 
 function Loading() {
 
--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import Loading from '../../components/Loading/Loading';
 import './NotFound.scss';
 
 function NotFound() {
 
     return (
-        <Loading />
+        <div className='page page--not-found'>
+            <h1 className='not-found__title'>Oops this page seems to be missing...</h1>
+            <Loading />
+            <h2 className='not-found__subtitle'><Link to='/'>Click here to return home</Link></h2>
+        </div>
     );
 }
 
