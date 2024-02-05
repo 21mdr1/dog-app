2e0362154d60f5222558a216bf3ce3e27e6cc173
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-04 21:04:06 -0500

    Create page not found boilerplate

---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import Loading from '../../components/Loading/Loading';
+import './NotFound.scss';
+
+function NotFound() {
+
+    return (
+        <Loading />
+    );
+}
+
+export default NotFound;
